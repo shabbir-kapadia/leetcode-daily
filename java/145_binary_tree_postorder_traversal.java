31a642a93bbbea516887157db53a994239bdfba2
Shabbir Kapadia <kapadia4@illinois.edu>
2023-07-04 10:54:41 -0700

    daily leetcode

---
+++ java/145_binary_tree_postorder_traversal.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    List<Integer> answer = new ArrayList<Integer>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return answer;
+        }
+
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        answer.add(root.val);
+
+        return answer;
+    }
+}
