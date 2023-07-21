5a7004e78126f1a53445ca7c6f130068d03b7e14
Shabbir Kapadia <kapadia4@illinois.edu>
2023-07-20 21:21:34 -0700

    daily leetcode

---
+++ top questions company/oracle/1047_removeAdjDuplicates.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = [('#',0)]
+        k = 2
+        for c in s:
+            if stack[-1][0] == c:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([c,1])
+                
+        return ''.join(c * k for c,k in stack)
+        
+        
+        
